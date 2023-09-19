44eac041da28d59d13100b77b435c2b60bf94a7c
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-19 17:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function myFunction() {
+  var enterBtn = document.getElementById("enterBtn");
+  var status = document.getElementById("status");
+	status.innerHTML = "<h1>Entered Metaverse</h1>";
+	
+}
